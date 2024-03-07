4dc9e4b4a2e3020c97b1f1cad8fdd980f605b890
Ayomide fajobi <ayomide@wumdrop.com>
2024-03-07 20:55:16 +0100

    client upload setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,54 +1,79 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { removeAuth, setAuth } from './stores/features/auths/authSlice';
-import { credit, debit } from './stores/features/wallets/walletSlice';
-import { useAppDispatch, useAppSelector } from './stores/features/hooks';
 
+import { FormEvent, LegacyRef, useRef } from 'react'
+import './App.css'
 
 function App() {
+
+  const name = useRef<string>(null!);
+  const file = useRef<File>(null);
+
+  const inputRef = useRef<HTMLInputElement>(null!);
+  const imgRef = useRef<HTMLImageElement>(null!);
+  
+  
+
+  const Submit = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      console.log(file.current)
+  }
+
+  const Preview = (e: FormEvent<HTMLInputElement>) => {
+    const reader = new FileReader();
+   
+    if(e.currentTarget.files.length > 0){
+      reader.readAsDataURL(e.currentTarget.files[0]);
+    }
+
+    reader.onload = ()=> {
+       ResizeImage(reader.result as string)
+    }
+  }
+
+  const ResizeImage = (base64Image: string) => {
+     const canvas = document.createElement("canvas");
+     const context = canvas.getContext("2d");
+     
+     const image = new Image()
+     image.src = base64Image;
+     
+     image.onload = () => {
+
+      canvas.width = image.width;
+      canvas.height = image.height
+
+      context?.drawImage(image, 0, 0)
  
-  const {
-    auth: $auth, 
-    wallet: $wallet
-  } = useAppSelector((state) => state);
-
-  const dispatch = useAppDispatch();
-
-  const LoginUser = () => {
-    localStorage.setItem("_access_token", "sfsfsdfsdfsdf");
-    dispatch(setAuth({name: "Henshaw", role: "ADMIN", isLoggedIn: true, token: "fghjgch"}));
+      canvas.toBlob(async (blob) => {
+        file.current = blob;
+        imgRef.current.src = URL.createObjectURL(blob as Blob)
+      }, "image/jpeg", 0.8)
+  
+     }
   }
-  const LogOutUser = () => {
-    dispatch(removeAuth(null!));
-    dispatch(debit(0));
-    localStorage.removeItem("_access_token");
-  }
-  const TopUp = () => dispatch(credit($wallet?.amount + 5));
-
 
   return (
-    <>
-      <div>
+    <main className='p-5'>
+        <form onSubmit={(e)=> Submit(e)} className='40% p-5'>
+          <div className='text-center'>
+               <img ref={imgRef} id='imgRef' src="" style={{width: "300px", height: "300px"}} alt="Preview" />
+          </div>
+          <br />
 
-        <h1>User {$auth?.name} has {$wallet.amount}</h1>
-        <div>
-           <button onClick={LoginUser}>Login</button>
-           <button onClick={LogOutUser}>Logout</button>
-           <button onClick={TopUp}>Top Up</button>
-        </div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-     
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+          <div className='form-group text-start'>
+                <button onClick={()=> inputRef.current?.click()} className='btn btn-light border py-3 px-5'>Select from computer</button>
+                <input ref={inputRef} type="file" hidden className='form-control' onChange={Preview} />
+          </div>
+
+          <div className='form-group py-2 text-start'>
+              <label htmlFor="">Name</label>
+              <input  type="text" className='form-control' onChange={(e) => name.current = e.currentTarget.value} />
+          </div>
+
+          <div className='text-start mt-3'>
+             <button>Upload</button>
+          </div>
+        </form>
+    </main>
   )
 }
 
